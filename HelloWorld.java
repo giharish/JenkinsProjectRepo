761b3e7cd62b076daf1f0783d2be5b2657de3684
root <root@ip-172-31-2-239.ap-south-1.compute.internal>
2022-06-21 05:48:30 +0000

    Jfrog Integration

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, Jenkins Project to demonstrate Git WebHook Integration"); }
+	System.out.println("Hello, Jenkins Project to demonstrate JFrog Integration"); }
 }
 
