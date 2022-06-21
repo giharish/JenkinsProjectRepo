125656f1acc62e6c6b22ac4f5105ad775bc27cba
root <root@ip-172-31-2-239.ap-south-1.compute.internal>
2022-06-21 06:00:27 +0000

    testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-	System.out.println("Hello, Jenkins Project to demonstrate JFrog Integration"); }
+	System.out.println("Hello World, Jenkins Project to demonstrate JFrog Integration"); }
 }
 
